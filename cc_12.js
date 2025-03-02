842a63705e4098ae70ea9f754518a3c1e3e65260
ashlym1 <ashlym3@usf.edu>
2025-03-02 17:21:48 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -2,7 +2,7 @@
 const dashboardById = document.getElementById("dashboard");    // selecting by ID 
 const dashboardByQuery = document.querySelector("#dashboard"); // selecting using the querySelector 
 
-//  Created a div to act as the metric card element using createElement 
+//  Created a div to act as the metric card element 
 const revenueCard = document.createElement("div");
 
 // Set class and Attributes for identification 
@@ -16,3 +16,14 @@ revenueCard.innerHTML = `
 `;
 // adding  the revenueCard that was created into the dashboard section of the page
 dashboardById.appendChild(revenueCard);
+
+// Task 2: Updating Dashboard Metrick 
+ const metricCards = document.querySelectorAll( ".metric-cad"); // selecting all the metric cards 
+ const metricCardArray={...metricCard};
+ metricCardArray.forEach(card => {
+        const title = card.querySelector("h3");
+        title.innerText += " - Updated";
+        card.style.backgroundColor = "mintcream";
+  });
+
+
